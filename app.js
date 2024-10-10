ded1ccea241ab0300293d4ec1df8162e8a683c32
shahzain46 <shahzain6016@gmail.com>
2024-10-10 12:13:55 +0500

    modify app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require("morgan")
+const connectDB = require("./DB/connectDB");
+
+const authRoutes = require('./src/routes/authRoutes');
+const forgetPasswordRoutes = require('./src/routes/forgetPasswordRoutes');
+const projectRoutes = require('./src/routes/projectRoutes');
+const questionRoutes = require('./src/routes/questionsRoutes');
+const userRoutes = require('./src/routes/userRoutes.js');
+const commentRoutes = require('./src/routes/commentRoutes.js');
+
+
+const PORT = process.env.PORT 
+app.use(morgan("dev"))
+app.use(bodyParser.json());
+app.use(cors( {origin: 'http://localhost:5173' }));
+
+
+app.use('/auth',authRoutes);
+app.use('/forgetpassword',forgetPasswordRoutes);
+app.use('/project',projectRoutes);
+app.use('/question',questionRoutes);
+app.use('/user',userRoutes);
+app.use('/comment',commentRoutes);
+
+const start = async () => {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT,()=>{
+        console.log(`running on port ${PORT}...`)
+    })
+}
+
+start()
+
